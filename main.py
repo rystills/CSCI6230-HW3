31c6aa34ab9236ad9c2f54236d0a6d9783333d45
Ryan Stillings <rystills@gmail.com>
2018-11-05 04:52:06 -0500

    cleanup + fix documentation

--- main.py
+++ main.py
@@ -1,28 +1,35 @@
-import math, numpy
+import numpy
 
 """apply Blum Goldwasser Probabilistic Algorithm (encryption) to the specified message
 @param m: the message to encrypt
-@param p: 
-@param a: 
-@param b: 
-@param x0: 
+@param p: first Blum prime
+@param q: second Blum prime
+@param x0: random seed 
 @returns: the result of encrypting the specified message with the specified parameters using the Blum Goldwasser algorithm
 """
-def BGPEnc(m,p=499,q=547,a=-57,b=52,x0=159201):
+def BGPEnc(m,p=499,q=547,x0=159201):
+    #calculations just following the algorithm
     n=p*q
     k = int(numpy.log2(n))
     h = int(numpy.log2(k))
     t = len(m)//h
-    
+    #mts stores the values m_i from 1 to t; stored as strings for easy bit splicing
     mts = [m[i*h:i*h+h] for i in range(t)]
     c = []
     for m in mts:
         x0 = pow(x0, 2, n)
+        #convert to a binary string to extract the last h elements
         pi = str(bin(x0))[-h:]
         for i in range(h):
+            #manual bitwise or implementation for strings of bits
             c.append(1 if pi[i] != str(m[i]) else 0)
     return c,pow(x0, 2, n)
 
+"""quick and dirty greatest common divisor calculation
+@param x: first value for divisor check
+@param y: second value for divisor check
+@returns: the greatest common divisor of x and y
+"""
 def gcd(x,y):
     smaller = y if x > y else x
     for i in range(1, smaller+1): 
@@ -32,13 +39,16 @@ def gcd(x,y):
 
 """apply Blum Goldwasser Probabilistic Algorithm (decryption) to the specified message
 @param m: the message to encrypt
-@param p: 
-@param a: 
-@param b: 
-@param x0: 
+@param x: x_t+1 value calculated from encryption step
+@param p: first Blum prime
+@param q: second Blum prime
+@param a: first provided value
+@param b: second provided value
+@param x0: random seed 
 @returns: the result of decrypting the specified message with the specified parameters using the Blum Goldwasser algorithm
 """
 def BGPDec(m,x,p=499,q=547,a=-57,b=52,x0=159201):
+    #calculations just following the algorithm
     n=p*q
     k = int(numpy.log2(n))
     h = int(numpy.log2(k))
@@ -49,23 +59,23 @@ def BGPDec(m,x,p=499,q=547,a=-57,b=52,x0=159201):
     u = pow(x, d1, p)
     v = pow(x, d2, q)
     x = (v*a*p + u*b*q) % n
-    
+    #mts stores the values m_i from 1 to t; stored as strings for easy bit splicing
     mts = [m[i*h:i*h+h] for i in range(t)]
     c = []
     for m in mts:
         x0 = pow(x0, 2, n)
+        #convert to a binary string to extract the last h elements
         pi = str(bin(x0))[-h:]
         for i in range(h):
+            #manual bitwise or implementation for strings of bits
             c.append(1 if pi[i] != str(m[i]) else 0)
     return c
-    
 
 def main():
     m=[1,0,0,1,1,1,0,0,0,0,0,1,0,0,0,0,1,1,0,0]
     enc,x0 = BGPEnc(m)
     dec = BGPDec(enc,x0)
-    print("  message: {0}\nencrypted: {1}\ndecrypted: {2}\noriginal = decrypted? {3}".format(m,enc,dec,m==dec))
+    print("              m: {0}\nciphertext C(m): {1}\n        D(C(m)): {2}\n    m = D(C(m))? {3}".format(m,enc,dec,m==dec))
     
-
 if __name__ == "__main__":
     main()
