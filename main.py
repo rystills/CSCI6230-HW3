2d2b56536d3ce09a9322deee029d4c9028bcbf5d
Ryan Stillings <rystills@gmail.com>
2018-11-05 04:34:55 -0500

    add decryption (both need testing + validation, but appear to be reversible)

--- main.py
+++ main.py
@@ -13,18 +13,22 @@ def BGPEnc(m,p=499,q=547,a=-57,b=52,x0=159201):
     k = int(numpy.log2(n))
     h = int(numpy.log2(k))
     t = len(m)//h
-    mts = []
-    for i in range(0,t):
-        mts.append(m[i*h:i*h+h])
-    #print(mts)
+    
+    mts = [m[i*h:i*h+h] for i in range(t)]
     c = []
     for m in mts:
         x0 = pow(x0, 2, n)
         pi = str(bin(x0))[-h:]
-        #print("pi:",pi)
         for i in range(h):
             c.append(1 if pi[i] != str(m[i]) else 0)
-    return c
+    return c,pow(x0, 2, n)
+
+def gcd(x,y):
+    smaller = y if x > y else x
+    for i in range(1, smaller+1): 
+        if((x % i == 0) and (y % i == 0)): 
+            g = i 
+    return g
 
 """apply Blum Goldwasser Probabilistic Algorithm (decryption) to the specified message
 @param m: the message to encrypt
@@ -34,18 +38,33 @@ def BGPEnc(m,p=499,q=547,a=-57,b=52,x0=159201):
 @param x0: 
 @returns: the result of decrypting the specified message with the specified parameters using the Blum Goldwasser algorithm
 """
-def BGPDec(m,x,t,p=499,q=547,a=-57,b=52,x0=159201):
-    return m
-
+def BGPDec(m,x,p=499,q=547,a=-57,b=52,x0=159201):
+    n=p*q
+    k = int(numpy.log2(n))
+    h = int(numpy.log2(k))
+    t = len(m)//h
+    
+    d1 = pow(((p+1)//4), t+1, p-1)
+    d2 = pow(((q+1)//4), t+1, q-1)
+    u = pow(x, d1, p)
+    v = pow(x, d2, q)
+    x = (v*a*p + u*b*q) % n
+    
+    mts = [m[i*h:i*h+h] for i in range(t)]
+    c = []
+    for m in mts:
+        x0 = pow(x0, 2, n)
+        pi = str(bin(x0))[-h:]
+        for i in range(h):
+            c.append(1 if pi[i] != str(m[i]) else 0)
+    return c
+    
 
 def main():
-    #store message in binary (lists were causing more trouble than they were worth)
     m=[1,0,0,1,1,1,0,0,0,0,0,1,0,0,0,0,1,1,0,0]
-    #m="10011100000100001100"
-    #m=int('10011100000100001100', 2)
-    enc = BGPEnc(m)
-    #print("  message: {0}\nencrypted: {1}".format(bin(m),bin(enc[0])))
-    print("  message: {0}\nencrypted: {1}".format(m,enc))
+    enc,x0 = BGPEnc(m)
+    dec = BGPDec(enc,x0)
+    print("  message: {0}\nencrypted: {1}\ndecrypted: {2}\noriginal = decrypted? {3}".format(m,enc,dec,m==dec))
     
 
 if __name__ == "__main__":
